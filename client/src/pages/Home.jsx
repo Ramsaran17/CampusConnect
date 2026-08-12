fc21bd0aacb0ba61618236d9c61d3993059138d1
DivviPavani <pavanidivvi1@gmail.com>
2026-08-12 16:14:39 +0530

    Add routing and initial pages

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import Navbar from '../components/Navbar'
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1>CampusConnect Home</h1>
+      <p>Welcome to the campus community.</p>
+    </div>
+  )
+}
+
+export default Home
